bf27e4b50ec551f736050c50f48820970d9c6a71
Dylan Attal <attal.dylan@gmail.com>
2022-01-31 17:10:17 -0500

    can delete tasks

--- src/App.js
+++ src/App.js
@@ -24,10 +24,18 @@ function App() {
     },
   ])
 
+  const deleteTask = (e, id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
     <div className='container'>
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        'No Tasks To Show'
+      )}
     </div>
   )
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,10 +1,10 @@
 import Task from './Task'
 
-const Tasks = ({ tasks }) => {
+const Tasks = ({ tasks, onDelete }) => {
   return (
     <div>
       {tasks.map((task, i) => {
-        return <Task key={i} task={task} />
+        return <Task key={i} task={task} onDelete={onDelete} />
       })}
     </div>
   )
